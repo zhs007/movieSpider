f15be8f812a493906f55960eb1178f6101e591a6
zhs007 <sssxueren@gmail.com>
2016-08-30 19:04:59 +0800

    test

--- movieproc/bin/ontimer_hour.js
+++ movieproc/bin/ontimer_hour.js
@@ -4,10 +4,10 @@ var shell = require('shelljs');
 
 shell.cd('../moviespider');
 shell.exec('/usr/local/bin/scrapy crawl cili006');
-shell.exec('/usr/local/bin/scrapy crawl dytt8');
-shell.exec('/usr/local/bin/scrapy crawl w6vhao');
-shell.exec('/usr/local/bin/scrapy crawl dytt8proc');
-shell.exec('/usr/local/bin/scrapy crawl w6vhaoproc');
-
-shell.cd('../movieproc');
-shell.exec('/root/.nvm/versions/node/v5.1.0/bin/node bin/movieproc.js');
+//shell.exec('/usr/local/bin/scrapy crawl dytt8');
+//shell.exec('/usr/local/bin/scrapy crawl w6vhao');
+//shell.exec('/usr/local/bin/scrapy crawl dytt8proc');
+//shell.exec('/usr/local/bin/scrapy crawl w6vhaoproc');
+//
+//shell.cd('../movieproc');
+//shell.exec('/root/.nvm/versions/node/v5.1.0/bin/node bin/movieproc.js');
